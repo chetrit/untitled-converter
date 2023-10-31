02d716956d90076b53f85c4d1ef032e4a9332ec8
Arnaud ISSOIRE <72009426+aIssoire@users.noreply.github.com>
2023-10-31 22:00:45 +0700

    feat(front): ⛳️add first version of sing-in page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { PageExample } from 'pages/PageExample/PageExample'
 import { PageNotFound } from 'pages/PageNotFound/PageNotFound'
+import SignIn from 'pages/sing-in/SignIn'
 
 const pathToNode: Array<{ path: string, Element: any }> = [
   { path: '/', Element: PageExample },
-  { path: '/example', Element: PageExample }
+  { path: '/example', Element: PageExample },
+  { path: '/sing-in', Element: SignIn }
 ]
 
 function App () {

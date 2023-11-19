eab519a6a957c2ad28eea03ca4ec5b2d08c86426
Rich <uzenetelnyelo@gmail.com>
2023-11-19 15:47:29 +0700

    Fix app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,54 @@
 // App.tsx
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CurrencyConverter from 'pages/Converter/Converter';
-import Favorite from 'pages/Favorite/Favorite';
-import { PageExample } from 'pages/PageExample/PageExample';
-import { PageNotFound } from 'pages/PageNotFound/PageNotFound';
-import Selection from 'pages/Selection/Selection';
-import SignIn from 'pages/sign-in/SignIn';
-import SignUp from 'pages/sign-up/SignUp';
-import Header from './components/Header';
-import test_selection from 'pages/Test_selection/test_selection';
+import React from 'react'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import CurrencyConverter from 'pages/Converter/Converter'
+import Favorite from 'pages/Favorite/Favorite'
+import { PageExample } from 'pages/PageExample/PageExample'
+import { PageNotFound } from 'pages/PageNotFound/PageNotFound'
+import Selection from 'pages/Selection/Selection'
+import SignIn from 'pages/sign-in/SignIn'
+import SignUp from 'pages/sign-up/SignUp'
+
+import { AuthProvider } from 'components/AuthContext'
+import ProtectedRoute from 'components/ProtectedRoute'
+
+import Header from './components/Header'
 
 const pathToNode: Array<{ path: string, Element: any }> = [
-  { path: '/selection', Element: Selection },
-  { path: '/test_selection', Element: test_selection },
+  { path: '/', Element: PageExample },
   { path: '/example', Element: PageExample },
-  { path: '/sign-in', Element: SignIn },
-  { path: '/sign-up', Element: SignUp },
+
   { path: '/converter/:currencyPair', Element: CurrencyConverter },
+
   { path: '/favorite', Element: Favorite },
-];
+  { path: '/selection', Element: Selection }
+]
 
-function App() {
-  const fakeUser = {
-    name: 'Jane Doe',
-    profilePicture: '/path-to-profile-picture.jpg', // replace with the actual path to the image
-  };
-
+function App () {
   return (
-    <div className={'App'}>
-      <BrowserRouter>
-        <Header user={fakeUser} />
-        <Routes>
-          {pathToNode.map(({ path, Element }, index) => (
-            <Route key={index} path={path} element={<Element />} />
-          ))}
-          <Route path={'*'} element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    <AuthProvider>
+      <div className={'App'}>
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            {pathToNode.map(({ path, Element }, index) => (
+              <Route key={index} path={path} element={
+                <ProtectedRoute>
+                  <Element/>
+                </ProtectedRoute>
+              }
+              />
+            ))}
+            <Route path={'/sign-in'} element={<SignIn/>}/>
+            <Route path={'/sign-up'} element={<SignUp/>}/>
+            <Route path={'*'} element={<PageNotFound/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/pages/Selection/Selection.tsx
+++ frontend/src/pages/Selection/Selection.tsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import SearchIcon from '@mui/icons-material/Search'
 import {
   Box,
   Card,
@@ -8,74 +11,70 @@ import {
   Typography,
   IconButton,
   InputBase,
-  Snackbar,
-} from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import SearchIcon from '@mui/icons-material/Search';
-import Alert from '@mui/material/Alert';
+  Snackbar
+} from '@mui/material'
+import Alert from '@mui/material/Alert'
+import { Link } from 'react-router-dom'
+
 import Curve from 'assets/images/curve.png' // Replace with the actual path
-type SnackbarSeverity = 'error' | 'success' | 'info' | 'warning';
+type SnackbarSeverity = 'error' | 'success' | 'info' | 'warning'
 
 const ExchangeRateList = () => {
-  const [rates, setRates] = useState<any>({}); // You can replace 'any' with the correct type
-  const [base, setBase] = useState<string>(''); // Assuming 'base' is a string
+  const [rates, setRates] = useState<any>({}) // You can replace 'any' with the correct type
+  const [base, setBase] = useState<string>('') // Assuming 'base' is a string
 
-  const [loading, setLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [favorites, setFavorites] = useState(new Set());
-  const [open, setOpen] = useState(false);
-  const [snackbarSeverity, setSnackbarSeverity] = useState<SnackbarSeverity>('success');
-  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [loading, setLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [favorites, setFavorites] = useState(new Set())
+  const [open, setOpen] = useState(false)
+  const [snackbarSeverity, setSnackbarSeverity] = useState<SnackbarSeverity>('success')
+  const [snackbarMessage, setSnackbarMessage] = useState('')
 
   useEffect(() => {
     const fetchRates = async () => {
       try {
-        const response = await fetch('http://localhost:8080/rates');
-        const data = await response.json();
-        console.log('Rates response:', data);
-        setRates(data.rates);
-        setBase(data.base);
-        setLoading(false);
+        const response = await fetch('http://localhost:8080/rates')
+        const data = await response.json()
+        console.log('Rates response:', data)
+        setRates(data.rates)
+        setBase(data.base)
+        setLoading(false)
       } catch (error) {
-        console.error('Error fetching rates:', error);
+        console.error('Error fetching rates:', error)
       }
-    };
+    }
 
-    fetchRates();
-  }, []);
+    fetchRates()
+  }, [])
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value.toLowerCase());
-  };
-  
+    setSearchQuery(event.target.value.toLowerCase())
+  }
 
   const addToFavorites = (pair: string) => {
-    setFavorites(new Set(favorites).add(pair));
-    setSnackbarSeverity('success');
-    setSnackbarMessage(`Added ${pair} to favorites`);
-  };
+    setFavorites(new Set(favorites).add(pair))
+    setSnackbarSeverity('success')
+    setSnackbarMessage(`Added ${pair} to favorites`)
+  }
 
   const removeFromFavorites = (pair: string) => {
-    const updatedFavorites = new Set(favorites);
-    updatedFavorites.delete(pair);
-    setFavorites(updatedFavorites);
-  };
-  
+    const updatedFavorites = new Set(favorites)
+    updatedFavorites.delete(pair)
+    setFavorites(updatedFavorites)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const renderCurrencyPairs = () => {
     if (loading) {
-      return <p>Loading...</p>;
+      return <p>Loading...</p>
     }
 
     const filteredCurrencyPairs = Object.entries(rates).filter(([currencyCode]) =>
       currencyCode.toLowerCase().includes(searchQuery)
-    );
-
+    )
 
     return (
       <Box
@@ -84,48 +83,49 @@ const ExchangeRateList = () => {
           flexWrap: 'wrap',
           justifyContent: 'flex-start',
           maxWidth: '1280px',
-          width: '100%',
+          width: '100%'
         }}
       >
         {filteredCurrencyPairs.map(([pair, base], index) => (
           <Link
-          key={pair} to={`/converter/EUR-${pair}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+            key={pair} to={`/converter/EUR-${pair}`} style={{ textDecoration: 'none', color: 'inherit' }}
+          >
             <Card sx={{ maxWidth: 345, m: 2 }}>
               <CardMedia
-                component="img"
+                component={'img'}
                 height={140}
                 image={Curve}
-                alt="Trading Curve"
+                alt={'Trading Curve'}
                 sx={{ filter: 'blur(5px)' }}
               />
               <CardContent
                 sx={{
                   display: 'flex',
                   justifyContent: 'space-between',
-                  alignItems: 'center',
+                  alignItems: 'center'
                 }}
               >
-                <Typography variant="h5" component="h2">
-                <>
-                  EUR-{pair}
-                </>
+                <Typography variant={'h5'} component={'h2'}>
+                  <>
+                    EUR-{pair}
+                  </>
                 </Typography>
                 <IconButton
                   onClick={() => {
                     favorites.has(pair)
                       ? removeFromFavorites(pair)
-                      : addToFavorites(pair);
+                      : addToFavorites(pair)
                   }}
                 >
-                  {favorites.has(pair) ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                  {favorites.has(pair) ? <FavoriteIcon/> : <FavoriteBorderIcon/>}
                 </IconButton>
               </CardContent>
             </Card>
           </Link>
         ))}
       </Box>
-    );
-  };
+    )
+  }
 
   return (
     <div>
@@ -139,17 +139,17 @@ const ExchangeRateList = () => {
           backgroundColor: 'common.white',
           marginLeft: 0,
           width: '100%',
-          height: '50px',
+          height: '50px'
         }}
       >
-        <SearchIcon sx={{ color: 'inherit' }} />
+        <SearchIcon sx={{ color: 'inherit' }}/>
         <InputBase
           placeholder={'Search…'}
           sx={{
             color: 'inherit',
             padding: '8px 8px 8px 0',
             paddingLeft: 'calc(1em + 32px)',
-            width: '50%',
+            width: '50%'
           }}
           inputProps={{ 'aria-label': 'search' }}
           onChange={handleSearchChange}
@@ -158,7 +158,7 @@ const ExchangeRateList = () => {
       <Box
         sx={{
           display: 'flex',
-          justifyContent: 'center',
+          justifyContent: 'center'
         }}
       >
         {renderCurrencyPairs()}
@@ -169,10 +169,7 @@ const ExchangeRateList = () => {
         </Alert>
       </Snackbar>
     </div>
-  );
-};
+  )
+}
 
-export default ExchangeRateList;
-
-
-
+export default ExchangeRateList
